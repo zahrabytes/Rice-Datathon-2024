8259ccdb7d3088c66f59aeff6b4541c46d57de01
Kishan Yerneni <145873290+KishanYern@users.noreply.github.com>
2024-01-21 04:29:28 -0600

    Update riceDatathonTp.py

--- CalebDev/riceDatathonTp.py
+++ CalebDev/riceDatathonTp.py
@@ -1,13 +1,17 @@
 from taipy.gui import Gui
+from taipy import Config
 from math import cos, exp
 from starknet_py.net.full_node_client import FullNodeClient
 from starknet_py.net.models.chains import StarknetChainId
 from starknet_py.net.signer.stark_curve_signer import KeyPair
 from starknet_py.contract import Contract
+import xgboost as xgb
+import pandas as pd
+import numpy as np
 
 content = ""
 img_path = "placeholder_image.png"
-prob = 0
+prob = "0"
 pred = ""
 chevron_path = r"Chevron_Logo.svg.png"
 plots = r"plots.png"
@@ -26,13 +30,38 @@ page = """
 
 <|{chevron_path}|image|>
 >
-<|{content}|file_selector|extensions=.csv|>
+<|{content}|file_selector|extensions=.csv|on_action=runMLAlgo|>
 Select a CSV file from your file system.
 
 ### Prediction
 <|{pred}|text|>
 Prediction Probability: <|{prob}|text|>
 """
+
+def runMLAlgo(state):
+    df = pd.read_csv(state.content)
+    if df.shape[0] != 1:
+        state.prob = "Error! Please upload a single row CSV file."
+        state.pred = ""
+        return
+    columns = ["surface_x", "surface_y", "bh_x", "bh_y", "gross_perforated_length", 
+               "total_proppant", "total_fluid", "true_vertical_depth", "ffs_frac_type", 
+               "proppant_intensity", "frac_fluid_intensity", "proppant_to_frac_fluid_ratio", 
+               "frac_fluid_to_proppant_ratio", "bin_lateral_length", "relative_well_position", 
+               "well_family_relationship", "frac_seasoning", "horizontal_midpoint_x", "horizontal_midpoint_y", 
+               "horizontal_toe_x", "horizontal_toe_y"]
+    
+    for col in columns:
+        if col not in df.columns:
+            state.prob = "Error! Please upload a file with the following columns: " + ", ".join(columns)
+            state.pred = ""
+            return
+        
+    xgb_model = xgb.Booster()
+    xgb_model.load_model("xgb_model_scaled.json")
+    xgb_pred = xgb_model.predict(xgb.DMatrix(df[columns]))
+    state.prob = str(xgb_pred[0])
+    
 
 async def on_button_press(state):
     try:
@@ -73,5 +102,8 @@ async def on_button_press(state):
 
 value = 0
 
-Gui(page).run(use_reloader=True, port=5001)
+app = Gui(page)
+
+if __name__ == "__main__":
+    app.run(use_reloader=True, port=5001)
 
